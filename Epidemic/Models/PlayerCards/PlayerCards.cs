6ffa454f79500f1fbfdb69f51bf0e381dde8721a
Prasanth <prasanthlouis21@gmail.com-at-383762989543>
2020-08-15 15:56:34 -0400

    Inital infra

---
+++ Epidemic/Models/PlayerCards/PlayerCards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epidemic.Models.PlayerCards
+{
+    public class PlayerCards
+    {
+        public Cities City { get; set; }
+        public Event Event { get; set; }
+        public Epidemic Epidemic { get; set; }
+    }
+}
---
+++ Epidemic/Objects/Pawns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Epidemic.Models.PlayerCards;
+using Microsoft.AspNetCore.Identity;
+
+namespace Epidemic.Objects
+{
+    public class Pawns
+    {
+        public Color Color;
+        public int MovementCounter { get; set; }
+        public Cities City { get; set; }
+        public Role Role { get; set; }
+        public Pawns()
+        {
+            MovementCounter = 4;
+        }
+    }
+}
